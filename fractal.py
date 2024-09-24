fe4301bb026551efd31a33404ab9226cb8e2500e
cristian <mcgmesajil@gmail.com>
2024-09-24 17:00:42 -0500

    fractal gpt

--- fractal.py
+++ fractal.py
@@ -5,14 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-A1=np.array([[0,0],[0,0.16]])
-A2=np.array([[0.85,0.04],[-0.04,0.85]])
-A3=np.array([[0.2,-0.26],[0.23,0.22]])
-A4=np.array([[-0.15,0.28],[0.26,0.24]])
-b1=np.array([[0],[0]])
-b2=np.array([[0],[1.6]])
-b3=np.array([[0],[1.6]])
-b4=np.array([[0],[0.44]])
+# Definir las matrices de transformación y los vectores
+A1 = np.array([[0, 0], [0, 0.16]])
+A2 = np.array([[0.85, 0.04], [-0.04, 0.85]])
+A3 = np.array([[0.2, -0.26], [0.23, 0.22]])
+A4 = np.array([[-0.15, 0.28], [0.26, 0.24]])
+
+b1 = np.array([[0], [0]])
+b2 = np.array([[0], [1.6]])
+b3 = np.array([[0], [1.6]])
+b4 = np.array([[0], [0.44]])
+
+# Clase principal de la ventana
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -50,46 +54,56 @@ class MainWindow(QMainWindow):
     def update_plot(self, value):
         n = int(value)
         self.ax.clear()
-        puntosx,puntosy=self.probabilidad()
-        #x = np.random.rand(n)
-        #y = np.random.rand(n)
-        self.ax.scatter(puntosx, puntosy, c='blue')
+        puntosx, puntosy = self.probabilidad(n)
+        self.ax.scatter(puntosx, puntosy, c='blue', s=0.1)
         self.canvas.draw()
 
     def change_drawing(self):
         n = self.slider.value()
         self.update_plot(n)
 
-        
-    def S1(self,vector):
-        return A[0]@vector+b[0]
-    def S2(self,vector):
-        return A[1]@vector+b[1]
-    def S3(self,vector):
-        return A[2]@vector+b[2]
-    def S4(self,vector):
-        return A[3]@vector+b[3]
-    S=[S1,S2,S3,S4]
+    # Funciones de transformación
+    def S1(self, vector):
+        return A1 @ vector + b1
 
-    def probabilidad(self):
-        # Definir los valores discretos y sus probabilidades asociadas
+    def S2(self, vector):
+        return A2 @ vector + b2
+
+    def S3(self, vector):
+        return A3 @ vector + b3
+
+    def S4(self, vector):
+        return A4 @ vector + b4
+
+    # Definir las funciones de transformación
+    S = [S1, S2, S3, S4]
+
+    def probabilidad(self, n_muestras):
+        # Valores y probabilidades
         valores = [1, 2, 3, 4]
         probabilidades = [0.01, 0.85, 0.07, 0.07]
 
         # Verificar que las probabilidades sumen 1
         assert np.isclose(np.sum(probabilidades), 1), "Las probabilidades deben sumar 1."
 
-        # Generar muestras aleatorias basadas en la distribución discreta
-        n_muestras = 100000  # Número de muestras a generar
-        muestras = np.random.choice(valores, size=n_muestras, p=probabilidades)
-        puntosx=[]
-        puntosy=[]
-        for i in range(0,n_muestras):
-            xx=self.S[muestras[i]-1](x)
-            puntosx.append(xx[0])
-            puntosy.append(xx[1])
-        return puntosx,puntosy
+        # Punto inicial
+        x = np.array([[0], [0]])
 
+        # Generar muestras aleatorias
+        muestras = np.random.choice(valores, size=n_muestras, p=probabilidades)
+        puntosx = []
+        puntosy = []
+
+        # Generar los puntos
+        for i in range(n_muestras):
+            transformacion = self.S[muestras[i] - 1](self, x)
+            x = transformacion
+            puntosx.append(x[0, 0])
+            puntosy.append(x[1, 0])
+
+        return puntosx, puntosy
+
+# Ejecutar la aplicación
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
